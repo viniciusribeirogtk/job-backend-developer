467215aa7cca99b716be3ec2fe7391b091d4874d
Vinicius Ribeiro <viniciusribeirosp@gmail.com>
2018-06-26 21:12:28 -0300

    enable swagger api

---
+++ job-backend-developer-user-details/src/main/java/br/com/intelipost/user/details/swagger/SwaggerConfig.java
@@ -0,0 +1,43 @@
+package br.com.intelipost.user.details.swagger;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * 
+ * @author Vinicius-Ribeiro
+ *
+ */
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket apiDoc() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                    .apis(RequestHandlerSelectors.any())
+                    .paths(PathSelectors.any())
+                    .build()
+                .apiInfo(metaData());
+    }
+
+    private ApiInfo metaData() {
+        return new ApiInfoBuilder()
+                .title("Job Backend Developer User Details")
+                .description("Api of User Details")
+                .version("1.0")
+                .contact(new Contact("Intelipost", "https://www.intelipost.com.br/", "contato@intelipost.com"))
+                .build();
+    }
+}
