858bf2b827df11e1ab9fe64d1655a53507bf2f44
Vinicius Ribeiro <viniciusribeirosp@gmail.com>
2018-06-26 21:04:48 -0300

    init project details

---
+++ job-backend-developer-user-details/src/main/java/br/com/intelipost/user/details/JobBackendDeveloperUserDetailsApplication.java
@@ -0,0 +1,12 @@
+package br.com.intelipost.user.details;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobBackendDeveloperUserDetailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobBackendDeveloperUserDetailsApplication.class, args);
+	}
+}
---
+++ job-backend-developer-user-details/src/test/java/br/com/intelipost/user/details/JobBackendDeveloperUserDetailsApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.intelipost.user.details;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JobBackendDeveloperUserDetailsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
